3fd2dfe94ac7162c1c4284b7c05998dcc231299c
microbit-carlos <carlos@microbit.org>
2020-06-14 17:58:35 +0000

    CI: Add build step again.

--- .github/workflows/scripts/install-toolchain-win.ps1
+++ .github/workflows/scripts/install-toolchain-win.ps1
@@ -5,5 +5,3 @@ choco install ninja
 # Install srec by downloading it and unzipping into the cwd
 C:\msys64\usr\bin\wget.exe -O srecord.zip https://sourceforge.net/projects/srecord/files/srecord-win32/1.64/srecord-1.64-win32.zip/download
 Expand-Archive -Force srecord.zip .
-# Have a look at the cwd to confirm all is good
-dir
